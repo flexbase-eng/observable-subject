f9e279bed9f7f604bbe6413cd7b33fb689af7c8b
Robert Dean <robert@flexbase.app>
2022-11-26 10:07:23 -0500

    better esm support

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
-import typescript from 'rollup-plugin-typescript2'
-import pkg from './package.json'
+import typescript from '@rollup/plugin-typescript'
+import pkg from './package.json' assert { type: 'json' };
 
 export default {
   input: 'src/index.ts',
@@ -8,13 +8,13 @@ export default {
   },
   {
     file: pkg.module, format: 'es',
-  }],
+  }
+  ],
   external: [
     ...Object.keys(pkg.dependencies || {}),
     ...Object.keys(pkg.peerDependencies || {})
   ],
   plugins: [
-    typescript({ typescript: require('typescript'), })
+    typescript()
   ],
 }
-

9f390c8fdf052c66251d2af740510ee3e1b3fae9
Robert Dean <robert@flexbase.app>
2023-03-16 13:06:26 -0400

    Update vitest.ci.config.ts

--- vitest.ci.config.ts
+++ vitest.ci.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    globals: true,
+    environment: 'node',
+    setupFiles: 'tests/setup.ts',
     coverage: {
       provider: 'istanbul',
       reporter: ['json', 'clover', 'lcov'],
